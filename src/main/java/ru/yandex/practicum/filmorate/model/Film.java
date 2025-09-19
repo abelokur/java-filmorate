2c4a9257050560301eccaf419d76e5b2861d02d3
Andrey A. Belokur <abelokur@yandex.ru>
2025-09-19 12:03:11 +0300

    Исправление замечаний ревью

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -11,10 +11,16 @@ import java.time.LocalDate;
  */
 @Data
 public class Film {
+
     private int id;
+
     private String name;
+
     private String description;
+
     private LocalDate releaseDate;
+
     @JsonSerialize(using = MyDurationSerializer.class)
     private Duration duration;
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -6,9 +6,15 @@ import java.time.LocalDate;
 
 @Data
 public class User {
+
     private int id;
+
     private String email;
+
     private String login;
+
     private String name;
+
     private LocalDate birthday;
+
 }
